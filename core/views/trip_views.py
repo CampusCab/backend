6f4b170bcf123eb6ab2e9c18bcd86381f6f0bcab
Emmanuel López Rodríguez <contact@emlopezr.com>
2024-05-01 21:49:06 -0500

    Start trip views

---
+++ core/views/trip_views.py
@@ -0,0 +1,13 @@
+# Ofrecer el viaje
+
+# Conseguir el viaje actual del usuario
+
+# Conseguir todos los viajes disponibles para el usuario
+
+# Aceptar oferta
+
+# Rechazar oferta
+
+# Terminar viaje siendo pasajero y calificar al conductor
+
+# Eliminar un usuario de un viaje y calificar al usuario. Si ya no hay pasajeros, terminar el viaje
