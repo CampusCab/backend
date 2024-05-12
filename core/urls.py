21c014cf408340abe061b1a273159426700fb1fb
Emmanuel López Rodríguez <contact@emlopezr.com>
2024-05-12 10:40:06 -0500

    Create endpoint URIs for trip views

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from .views import vehicle_views
-
+from .views import vehicle_views, trip_views
 
 urlpatterns = [
     path('vehicles/', vehicle_views.get_user_vehicles, name = 'get_user_vehicles'),
     path('vehicles/create', vehicle_views.create_vehicle, name = 'create_vehicle'),
+    path('trips/avaliables', trip_views.get_available_trips, name = 'get_available_trips'),
+    path('trips/current', trip_views.get_current_trip, name = 'get_current_trip'),
+    path('trips/past', trip_views.get_driver_trips, name = 'get_driver_trips'),
+    path('trips/create', trip_views.create_trip, name = 'create_trip'),
+    path('trips/<int:trip_id>/accept', trip_views.accept_offer, name = 'accept_offer'),
+    path('trips/<int:trip_id>/reject', trip_views.reject_offer, name = 'reject_offer'),
+    path('trips/<int:trip_id>/finish', trip_views.finish_trip_as_passenger, name = 'finish_trip_as_passenger'),
+    path('trips/<int:trip_id>/remove/<int:passenger_id>', trip_views.remove_user_from_trip, name = 'remove_user_from_trip'),
 ]
--- core/views/trip_views.py
+++ core/views/trip_views.py
@@ -1,13 +1,53 @@
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+
 # Ofrecer el viaje
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def create_trip(request):
+    return JsonResponse({"message": "Create trip"}, status=status.HTTP_200_OK)
 
 # Conseguir el viaje actual del usuario
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def get_current_trip(request):
+    return JsonResponse({"message": "Get current trip"}, status=status.HTTP_200_OK)
 
 # Conseguir todos los viajes disponibles para el usuario
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def get_available_trips(request):
+    return JsonResponse({"message": "Get available trips"}, status=status.HTTP_200_OK)
 
 # Aceptar oferta
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def accept_offer(request):
+    return JsonResponse({"message": "Accept offer"}, status=status.HTTP_200_OK)
 
 # Rechazar oferta
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def reject_offer(request):
+    return JsonResponse({"message": "Reject offer"}, status=status.HTTP_200_OK)
 
 # Terminar viaje siendo pasajero y calificar al conductor
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def finish_trip_as_passenger(request):
+    return JsonResponse({"message": "Finish trip as passenger"}, status=status.HTTP_200_OK)
 
 # Eliminar un usuario de un viaje y calificar al usuario. Si ya no hay pasajeros, terminar el viaje
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def remove_user_from_trip(request):
+    return JsonResponse({"message": "Remove user from trip"}, status=status.HTTP_200_OK)
+
+# Conseguir todos los viajes en los que el usuario ha sido conductor, total recogido y deuda con la plataforma
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def get_driver_trips(request):
+    return JsonResponse({"message": "Get driver trips"}, status=status.HTTP_200_OK)
