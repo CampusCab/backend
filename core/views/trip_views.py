7efddce529cdcb3613f2049ad30ae72c1b522319
emlopezr <lopezrodemmanuel@gmail.com>
2024-08-15 20:16:36 -0500

    Add capacity to trip

--- core/views/trip_views.py
+++ core/views/trip_views.py
@@ -69,7 +69,7 @@ def get_current_trip(request):
         data = data | {"accepted": offer.accepted}
     elif user.currently_driver:
         offers = user.current_trip_driver.offer_set.all()
-        data = data | {"offers": CurrentOfferSerializer(offers, many=True).data}
+        data = data | {"offers": CurrentOfferSerializer(offers, many=True).data} | {"capacity": user.current_trip_driver.vehicle.max_passengers }
 
     return JsonResponse(data, status=status.HTTP_200_OK)
 
